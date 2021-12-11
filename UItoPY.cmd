bf00a6f6e2b9a498017f4de0c167b26cc5cce127
gsak3l <gsakel5@gmail.com>
2021-12-11 10:39:01 +0200

    fixed bug with new images not appearing when i ran the python file

--- UItoPY.cmd
+++ UItoPY.cmd
@@ -1,1 +1,2 @@
-C:\Users\gsak3\AppData\Local\Programs\Python\Python39\Scripts\pyside6-uic modules\ui_main.ui > modules\ui_main.py
+REM C:\Users\gsak3\AppData\Local\Programs\Python\Python39\Scripts\pyside6-uic modules\ui_main.ui > modules\ui_main.py
+C:\Users\gsak3\AppData\Local\Programs\Python\Python39\Scripts\pyside6-rcc.exe resources.qrc > modules\resources_rc.py
--- modules/ui_main.py
+++ modules/ui_main.py
@@ -593,7 +593,7 @@ class Ui_MainWindow(object):
         self.topLogo.setGeometry(QRect(10, 5, 42, 42))
         self.topLogo.setMinimumSize(QSize(42, 42))
         self.topLogo.setMaximumSize(QSize(42, 42))
-        self.topLogo.setStyleSheet(u"background-image:url(:/images/images/images/PyDracula.png);")
+        self.topLogo.setStyleSheet(u"background-image:url(:/images/images/images/ball_logo.png);")
         self.topLogo.setFrameShape(QFrame.NoFrame)
         self.topLogo.setFrameShadow(QFrame.Raised)
         self.titleLeftApp = QLabel(self.topLogoInfo)
@@ -693,9 +693,6 @@ class Ui_MainWindow(object):
         self.btn_import_video.setCursor(QCursor(Qt.PointingHandCursor))
         self.btn_import_video.setLayoutDirection(Qt.LeftToRight)
         self.btn_import_video.setStyleSheet(u"background-image: url(:/icons/images/icons/cil-movie.png);")
-        icon = QIcon()
-        icon.addFile(u":/icons/images/icons/cil-move.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.btn_import_video.setIcon(icon)
 
         self.verticalLayout_8.addWidget(self.btn_import_video)
 
@@ -761,9 +758,9 @@ class Ui_MainWindow(object):
         self.extraCloseColumnBtn.setMinimumSize(QSize(28, 28))
         self.extraCloseColumnBtn.setMaximumSize(QSize(28, 28))
         self.extraCloseColumnBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        icon1 = QIcon()
-        icon1.addFile(u":/icons/images/icons/icon_close.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.extraCloseColumnBtn.setIcon(icon1)
+        icon = QIcon()
+        icon.addFile(u":/icons/images/icons/icon_close.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.extraCloseColumnBtn.setIcon(icon)
         self.extraCloseColumnBtn.setIconSize(QSize(20, 20))
 
         self.extraTopLayout.addWidget(self.extraCloseColumnBtn, 0, 2, 1, 1)
@@ -914,9 +911,9 @@ class Ui_MainWindow(object):
         self.minimizeAppBtn.setMinimumSize(QSize(28, 28))
         self.minimizeAppBtn.setMaximumSize(QSize(28, 28))
         self.minimizeAppBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        icon2 = QIcon()
-        icon2.addFile(u":/icons/images/icons/icon_minimize.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.minimizeAppBtn.setIcon(icon2)
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/images/icons/icon_minimize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.minimizeAppBtn.setIcon(icon1)
         self.minimizeAppBtn.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_2.addWidget(self.minimizeAppBtn)
@@ -932,9 +929,9 @@ class Ui_MainWindow(object):
         font3.setStyleStrategy(QFont.PreferDefault)
         self.maximizeRestoreAppBtn.setFont(font3)
         self.maximizeRestoreAppBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        icon3 = QIcon()
-        icon3.addFile(u":/icons/images/icons/icon_maximize.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.maximizeRestoreAppBtn.setIcon(icon3)
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/images/icons/icon_maximize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.maximizeRestoreAppBtn.setIcon(icon2)
         self.maximizeRestoreAppBtn.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_2.addWidget(self.maximizeRestoreAppBtn)
@@ -944,7 +941,7 @@ class Ui_MainWindow(object):
         self.closeAppBtn.setMinimumSize(QSize(28, 28))
         self.closeAppBtn.setMaximumSize(QSize(28, 28))
         self.closeAppBtn.setCursor(QCursor(Qt.PointingHandCursor))
-        self.closeAppBtn.setIcon(icon1)
+        self.closeAppBtn.setIcon(icon)
         self.closeAppBtn.setIconSize(QSize(20, 20))
 
         self.horizontalLayout_2.addWidget(self.closeAppBtn)
@@ -1050,9 +1047,9 @@ class Ui_MainWindow(object):
         self.pushButton.setFont(font)
         self.pushButton.setCursor(QCursor(Qt.PointingHandCursor))
         self.pushButton.setStyleSheet(u"background-color: rgb(52, 59, 72);")
-        icon4 = QIcon()
-        icon4.addFile(u":/icons/images/icons/cil-folder-open.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.pushButton.setIcon(icon4)
+        icon3 = QIcon()
+        icon3.addFile(u":/icons/images/icons/cil-folder-open.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon3)
 
         self.gridLayout.addWidget(self.pushButton, 0, 1, 1, 1)
 
@@ -1171,9 +1168,9 @@ class Ui_MainWindow(object):
         self.commandLinkButton.setObjectName(u"commandLinkButton")
         self.commandLinkButton.setCursor(QCursor(Qt.PointingHandCursor))
         self.commandLinkButton.setStyleSheet(u"")
-        icon5 = QIcon()
-        icon5.addFile(u":/icons/images/icons/cil-link.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.commandLinkButton.setIcon(icon5)
+        icon4 = QIcon()
+        icon4.addFile(u":/icons/images/icons/cil-link.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.commandLinkButton.setIcon(icon4)
 
         self.gridLayout_2.addWidget(self.commandLinkButton, 1, 6, 1, 1)
 
@@ -1329,7 +1326,7 @@ class Ui_MainWindow(object):
         self.btn_local_footage = QPushButton(self.new_page)
         self.btn_local_footage.setObjectName(u"btn_local_footage")
         self.btn_local_footage.setMaximumSize(QSize(80, 80))
-        self.btn_local_footage.setStyleSheet(u"background-image:url(:/icons/images/icons/cil-home.png);\n"
+        self.btn_local_footage.setStyleSheet(u"background-image:url(:/images/images/images/folder.png);\n"
                                              "/* border: none; */\n"
                                              "background-repeat:no-repeat;\n"
                                              "margin-top:10px")
