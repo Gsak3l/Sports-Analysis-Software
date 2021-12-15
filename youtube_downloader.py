34487e1ee46b1ea5526ace785c1f8ee5ae071e91
gsak3l <gsakel5@gmail.com>
2021-12-15 10:34:08 +0200

    improving youtube downloader

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,10 +1,42 @@
+import time
+
 import pytube
-from pathlib import Path
+import os
 
-downloads_path = str(Path.home() / "Downloads")
 
-url = 'https://www.youtube.com/watch?v=jXjUkN-J7kE'
+# Saving the youtube video, might add on progress bar in the future I guess...
+def save_video_to_downloads(url):
+    youtube = pytube.YouTube(url)
+    youtube_video_title = youtube.title
 
-youtube = pytube.YouTube(url)
-video = youtube.streams.get_highest_resolution()
-video.download(downloads_path)
+    youtube = pytube.YouTube(url)
+    video = youtube.streams.get_highest_resolution()
+    video.download(file_path())
+
+
+# On complete seems to work just fine
+def on_complete(yt_title):
+    path = file_path() + '\\' + yt_title + '.mp4'
+    if os.path.exists(path):
+        return False
+
+
+def on_progress(yt_title):
+    i = 0
+    while not on_complete(yt_title):
+        time.sleep(1)
+        print(str(i) + "%")
+        i = i + 0.1
+    i = 100
+    print(str(i) + "%")
+
+
+# Downloading the file and saving it to the Downloads
+def file_path():
+    home = os.path.expanduser('~')
+    download_path = os.path.join(home, 'Downloads')
+    return download_path
+
+
+flag = True
+save_video_to_downloads('https://www.youtube.com/watch?v=Cw7AArqO-CE')
