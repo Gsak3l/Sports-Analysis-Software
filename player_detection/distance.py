13e923cf51819090d8f545ec7cd3b6695d2a485d
gsak3l <gsakel5@gmail.com>
2022-01-16 21:14:10 +0200

    FIXME this is not the right way to calculate meter distance between two points distance calculation between players in meters doesn't work the way it is supposed to, have to find a different approach

--- player_detection/distance.py
+++ player_detection/distance.py
@@ -12,59 +12,66 @@ def read_and_clean(file):
     return df2
 
 
-def euclidean_distance_pixels(df_3):
+def euclidean_distance_pixels(df3):
     # works but 10gb of ram doesn't seem good
     x2_x1 = []
     y2_y1 = []
-    for i in range(df_3.shape[0]):
-        x2_x1.append([df_3['x'].loc[i] - df_3['x'].loc[j] for j in range(df_3.shape[0])])
-        y2_y1.append([df_3['y'].loc[i] - df_3['y'].loc[j] for j in range(df_3.shape[0])])
+    for i in range(df3.shape[0]):
+        x2_x1.append([df3['x'].loc[i] - df3['x'].loc[j] for j in range(df3.shape[0])])
+        y2_y1.append([df3['y'].loc[i] - df3['y'].loc[j] for j in range(df3.shape[0])])
 
     # basically x2-x1 and y2-y1
     x2_x1 = pd.DataFrame(x2_x1)
     y2_y1 = pd.DataFrame(y2_y1)
 
     df_player_distance_ = []
-    for i in range(df_3.shape[0]):
+    for i in range(df3.shape[0]):
         df_player_distance_.append(
-            [sqrt(pow(x2_x1.iloc[i][j], 2) + pow(y2_y1.iloc[i][j], 2)) for j in range(df_3.shape[0])])
+            [sqrt(pow(x2_x1.iloc[i][j], 2) + pow(y2_y1.iloc[i][j], 2)) for j in range(df3.shape[0])]
+        )
 
     df_player_distance_ = pd.DataFrame(df_player_distance_)
+    df_player_distance_ = df_player_distance_.astype(int)
     return df_player_distance_
 
 
 def players_from_camera_meters(df4):
-    df4['CM'] = pd.DataFrame(np.random.randint(175, 185, size=(df4.shape[0], 1)))
+    df4['CM'] = pd.DataFrame(np.random.randint(175, 190, size=(df4.shape[0], 1)))
     df4.loc[df4['w'] > 60, 'CM'] = (732 + 244) / 2  # goalpost6
-    f_mm = 30
+    f_mm = 35
     image_height_px = 720
-    sensor_height_mm = 22
+    sensor_height_mm = 44.3
 
-    df4['DC'] = ((f_mm * df4['CM'] * 10 * image_height_px) / (df4['h'] * sensor_height_mm))
+    # cm * 10 to get the mm
+    df4['DC'] = (f_mm * df4['CM'] * 10 * image_height_px) / (df4['h'] * sensor_height_mm)
     df4['DC'] = df4['DC'].astype(int)
 
     return df4
 
 
-def pixels_to_meters(df5, df_distance_px):
-    known_pixel_distance = df_distance_px.iloc[1][16]
-    known_meters = 9.15
-    df_meters = df_distance_px / 5
-    print(df_meters.iloc[1][8])
-    print(df5)
+def euclidean_distance_meters(df5, distance_pixel):
+    distance_meters = []
+    for i in range(df5.shape[0]):
+        # FIXME this is not the right way to calculate meter distance between two points
+        distance_meters.append([(df5.loc[i]['DC'] * distance_pixel.iloc[i][j]) for j in range(df5.shape[0])])
+
+    distance_meters = pd.DataFrame(distance_meters)
+    return distance_meters
 
 
 def manager(df, frame):
     df = read_and_clean(df)
     df = df[df['Frame'] == frame]
-    df_player_distance = euclidean_distance_pixels(df)
+    df_player_distance_px = euclidean_distance_pixels(df)
     df = players_from_camera_meters(df)
-    pixels_to_meters(df, df_player_distance)
+    df_player_distance_meters = euclidean_distance_meters(df, df_player_distance_px)
+    print(df_player_distance_meters.iloc[1][16])
+    print(df)
 
 
 if __name__ == '__main__':
     desired_width = 320
     pd.set_option('display.width', desired_width)
     np.set_printoptions(linewidth=desired_width)
-    pd.set_option('display.max_columns', 20)
+    pd.set_option('display.max_columns', 23)
     manager('runs/track/exp6/Tactical View- Pixellot C Coaching.txt', 3)
