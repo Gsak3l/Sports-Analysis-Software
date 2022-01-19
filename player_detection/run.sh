6271ee5918f1985e90db196fe05cf4afe20eb1a9
gsak3l <gsakel5@gmail.com>
2022-01-19 12:47:07 +0200

    new weights added

--- player_detection/run.sh
+++ player_detection/run.sh
@@ -1,18 +1,14 @@
 # training using yolov5
-cd yolov5
-python3 train.py --img 640 --batch 24 --batch-size 24 --epochs 800 \
---data custom_data.yaml --weights yolov5s.pt --nosave --cache --patience 0
-
+#cd yolov5
+#python3 train.py --img 640 --batch 24 --batch-size 24 --epochs 800 \
+#--data custom_data.yaml --weights yolov5s.pt --nosave --cache
 
 # detection using yolov5
-#python3 yolov5/detect.py --source 'videos/Tactical View- Pixellot C Coaching.mp4' \
-#--weights yolov5/runs/train/exp3/weights/last.pt --hide-labels --classes 0 1 2 3 4 --save-txt
-
+#cd yolov5
+#python3 detect.py --source 'videos/Tactical View- Pixellot C Coaching.mp4' \
+#--weights runs/train/exp3/weights/last.pt --hide-labels --classes 0 1 2 3 4 --save-txt
 
 # detection using deepsort with the weights of yolov5
-#python3 track.py --yolo_mode yolov5/runs/train/exp3/weights/last.pt \
-#--source 'videos/Tactical View- Pixellot C Coaching.mp4' --classes 0 1 2 3 4 \
-# --save-vid --save-txt
-
-
-# stock yolov5 deeposort
+#python3 track.py --yolo_mode yolov5/runs/train/exp45/weights/last.pt \
+#  --source 'videos/Tactical View- Pixellot C Coaching.mp4' --classes 0 1 2 3 4 5 6 \
+#  --save-vid --save-txt
