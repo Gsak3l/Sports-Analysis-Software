91239a9b3981346eda9cfe9938d0c8ed5a5badcd
gsak3l <gsakel5@gmail.com>
2022-01-23 20:07:22 +0200

    789 epochs with max id 115 instead of 219 with 300 epochs, all labels added

--- player_detection/run.sh
+++ player_detection/run.sh
@@ -1,6 +1,6 @@
 # training using yolov5
 #cd yolov5
-#python3 train.py --img 640 --batch 24 --batch-size 24 --epochs 800 \
+#python3 train.py --img 640 --batch 24 --batch-size 24 --epochs 1200 \
 #--data custom_data.yaml --weights yolov5s.pt --nosave --cache
 
 # detection using yolov5
@@ -9,6 +9,6 @@
 #--weights runs/train/exp3/weights/last.pt --hide-labels --classes 0 1 2 3 4 --save-txt
 
 # detection using deepsort with the weights of yolov5
-python3 track.py --yolo_mode yolov5/runs/train/exp46/weights/best.pt \
-  --source 'videos/Tactical View- Pixellot C Coaching.mp4' --classes 0 \
-  --save-vid --save-txt
+#python3 track.py --yolo_mode yolov5/runs/train/exp49/weights/last.pt \
+#  --source 'videos/Tactical View- Pixellot C Coaching.mp4' --classes 0 1 \
+#  --save-vid --save-txt
