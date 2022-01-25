8f17c1b3591a057a6c1d025fbd6346fb13552fcf
gsak3l <gsakel5@gmail.com>
2022-01-25 21:11:15 +0200

    added running distance for players, trained dataset for 2400 epcohs 115->90

--- player_detection/distance.py
+++ player_detection/distance.py
@@ -12,6 +12,7 @@ def read_and_clean(file):
     return df2
 
 
+# calculate distance between players in pixels using the euclidean distance
 def euclidean_distance_pixels(df3):
     x2_x1 = []
     y2_y1 = []
@@ -35,28 +36,10 @@ def euclidean_distance_pixels(df3):
     return df_player_distance_
 
 
-def players_from_camera_meters(df4):
-    df4['CM'] = pd.DataFrame(np.random.randint(175, 190, size=(df4.shape[0], 1)))
-    df4.loc[df4['h'] > 100, 'CM'] = 5000  # goalpost6
-    f_mm = 35
-    image_height_px = 720
-    sensor_height_mm = 44.3
-
-    # I don't know what I am doing, I really don't
-    df4.loc[df4['h'] * .8 > df4['w'], 'DC'] = \
-        (f_mm * df4['CM'] * 10 * image_height_px) / ((df4['h'] + df4['w']) * sensor_height_mm)
-
-    df4.loc[df4['h'] * .8 > df4['w'], 'DC'] = \
-        (f_mm * df4['CM'] * 10 * image_height_px) / ((df4['h'] * 0.4 + df4['w'] * 0.6) * sensor_height_mm)
-
-    df4['DC'] = df4['DC'] / 1000
-    df4['DC'] = df4['DC'].astype(int)
-
-    return df4
-
-
+# calculate distance between players in meters using the euclidean distance
 def euclidean_distance_meters(df5, distance_pixel):
     distance_meters = []
+
     for i in range(df5.shape[0]):
         # if anyone is reading this, please don't judge me, I have no idea what I am doing
         distance_meters.append(
@@ -70,40 +53,81 @@ def euclidean_distance_meters(df5, distance_pixel):
     return distance_meters
 
 
-def calculate_offense_defense(df_offense):
-    possession_changed = []
-    df_center = df_offense[(df_offense['h'] > 200)]
-    df_center = df_center.drop(['ID', 'y', 'w', 'h'], axis=1)
+def players_from_camera_meters(df4):
+    df4['CM'] = pd.DataFrame(np.random.randint(175, 190, size=(df4.shape[0], 1)))
+    df4.loc[df4['h'] > df4['h'].mean() * 2, 'CM'] = 5000  # goalpost
+    f_mm = 35
+    image_height_px = 720
+    sensor_height_mm = 44.3
 
-    # df_offense.loc[(df_offense['ID'] == 1) & (df_offense['x'] < 800), 'D/O'] = 'DEFENCE'
-    # print(df_offense.loc[(df_offense['ID'] == 1) & (df_offense['x'] > 800)])
+    # I don't know what I am doing, I really don't
+    df4.loc[df4['h'] * .8 > df4['w'], 'DC'] = \
+        (f_mm * df4['CM'] * 10 * image_height_px) / ((df4['h'] + df4['w']) * sensor_height_mm)
+
+    df4.loc[df4['h'] * .8 <= df4['w'], 'DC'] = \
+        (f_mm * df4['CM'] * 10 * image_height_px) / ((df4['h'] * 0.4 + df4['w'] * 0.6) * sensor_height_mm)
+
+    df4['DC'] = df4['DC'] / 1000
+    df4['DC'] = df4['DC'].astype(int)
+
+    return df4
+
+
+# calculating if the red team is on offense or on defense
+def calculate_offense_defense(df5):
+    possession_changed = []
+
+    df_center = df5[(df5['h'] > df5['h'].mean() * 2)]
+    df_center = df_center.drop(['ID', 'y', 'w', 'h'], axis=1)
 
     df_center.loc[df_center['x'] < 350, 'D/O'] = 'DEFENSE'
     df_center.loc[df_center['x'] > 800, 'D/O'] = 'OFFENSE'
     df_center['D/O'] = df_center['D/O'].replace(np.nan, 'JUST PLAYING')
 
-    # print(df_center[df_center['D/O'] == 'OFFENSE'])
-    # print(df_center[df_center['D/O'] == 'DEFENCE'])
-    # print(df_center[df_center['D/O'] == 'JUST PLAYING'])
-
     for i in range(df_center.shape[0] - 1):
         if df_center.iloc[i]['D/O'] != df_center.iloc[i + 1]['D/O']:
-            print('CHANGE', i)
-            print(df_center.iloc[i]['D/O'], i)
             possession_changed.append(i)
 
-    print(possession_changed)
     return possession_changed
 
 
+# def distance_from_center_meters(df7):
+#     f_mm = 35
+#     image_height_px = 720
+#     sensor_height_mm = 44.3
+#
+#     df7['D_CEN'] = (f_mm * df4['CM'] * 10 * image_height_px) / ((df4['h'] + df4['w']) * sensor_height_mm)
+
+
+# calculating running distance for each player
+def calculate_running_distance(df6):
+    df6.drop(df6[df6['h'] > df6['h'].mean() * 2].index, inplace=True)  # dropping the center of the football field
+    df_player_location = df6.drop(['Frame', 'x', 'y', 'w', 'h', 'CM'], axis=1)
+
+    running_distance = pd.DataFrame()
+    running_distance['x'] = df_player_location.groupby('ID')['DC'].sum() / 100
+
+    running_distance['x'] = running_distance['x'].astype(int)
+
+    print(running_distance)
+    return running_distance
+
+
 def manager(df, frame):
-    df = read_and_clean(df)
-    calculate_offense_defense(df)
+    df_ = df = read_and_clean(df)
+
+    od_timestamps = calculate_offense_defense(df)
+
     df = df[df['Frame'] == frame]
     df = df.reset_index(drop=True)
-    df_ds_px = euclidean_distance_pixels(df)  # distance between players pixels
+    euclidean_px = euclidean_distance_pixels(df)  # distance between players pixels
+
     df = players_from_camera_meters(df)
-    df_ds_m = euclidean_distance_meters(df, df_ds_px)  # distance between players meters
+    df_ = players_from_camera_meters(df_)
+
+    euclidean_m = euclidean_distance_meters(df, euclidean_px)  # distance between players meters
+
+    calculate_running_distance(df_)
 
 
 if __name__ == '__main__':
@@ -111,4 +135,4 @@ if __name__ == '__main__':
     pd.set_option('display.width', desired_width)
     np.set_printoptions(linewidth=desired_width)
     pd.set_option('display.max_columns', 23)
-    manager('runs/track/exp18/Tactical View- Pixellot C Coaching.txt', 46)
+    manager('runs/track/exp19/Tactical View- Pixellot C Coaching.txt', 46)
