c0cb63a0b965baacef551a0bd7d43582d26f426c
gsak3l <gsakel5@gmail.com>
2022-02-08 11:51:33 +0200

    zoom algorithm added using opencv zoo&crop

--- zoom_into_video.py
+++ zoom_into_video.py
@@ -1,48 +1,17 @@
-import cv2
-import numpy as np
+import cv2 as cv
 
 
-def cv2_clipped_zoom(img, zoom_factor=2):
-    """
-    Center zoom in/out of the given image and returning an enlarged/shrinked view of
-    the image without changing dimensions
-    ------
-    Args:
-        img : ndarray
-            Image array
-        zoom_factor : float
-            amount of zoom as a ratio [0 to Inf). Default 0.
-    ------
-    Returns:
-        result: ndarray
-           numpy ndarray of the same shape of the input img zoomed by the specified factor.
-    """
-    if zoom_factor == 0:
-        return img
-
-    height, width = img.shape[:2]  # It's also the final desired shape
-    new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
-
-    ### Crop only the part that will remain in the result (more efficient)
-    # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
-    y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
-    y2, x2 = y1 + height, x1 + width
-    bbox = np.array([y1, x1, y2, x2])
-    # Map back to original image coordinates
-    bbox = (bbox / zoom_factor).astype(np.int)
-    y1, x1, y2, x2 = bbox
-    cropped_img = img[y1:y2, x1:x2]
-
-    # Handle padding when downscaling
-    resize_height, resize_width = min(new_height, height), min(new_width, width)
-    pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) // 2
-    pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
-    pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0, 0)] * (img.ndim - 2)
-
-    result = cv2.resize(cropped_img, (resize_width, resize_height))
-    result = np.pad(result, pad_spec, mode='constant')
-    assert result.shape[0] == height and result.shape[1] == width
-    return result
+def zoom(img, zoom_factor=2):
+    return cv.resize(img, None, fx=zoom_factor, fy=zoom_factor)
 
 
-cv2_clipped_zoom(cv2.imread('C:/Users/gsak3/Desktop/frames/scene00046.png'), 2)
+def manager(img, x, y):
+    img = cv.imread(img)
+    cropped = img[y - 20: y + 50, x: x + 100]  # from y1 to y2 [y1:y2], from x1 to x2 [x1:x2]
+    img = zoom(img, 20)
+    zoomed_and_cropped = zoom(cropped, 10)
+    cv.imshow('', zoomed_and_cropped)
+    cv.waitKey(0)
+
+
+manager('C:/Users/gsak3/Desktop/frames/scene00046.png', 560, 305)
